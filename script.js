59a22303c00d2a7c68a13839c0fa05a3d9c173bc
LAK <email@lak.com>
2022-02-16 20:21:50 -0300

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const startBtn = document.querySelector('.start-btn')
 const stopBtn = document.querySelector('.stop-btn')
 const modal = document.querySelector('.modal')
 const time = document.querySelector('.time')
+const img = document.querySelector('.reference-img')
 
 startBtn.addEventListener('click', () => {
     time.innerHTML = `<span>${interval.value}:00</span>`
@@ -13,8 +14,12 @@ stopBtn.addEventListener('click', () => {
     modal.classList.toggle('change')
 })
 
-let timer = interval.value * 60
+let timer = interval.value * 2
 function countdown(){
+    if(timer === 0){
+        timer = 300
+        img.src = "ley-bowen-.jpg"
+    }
     time.innerHTML = `<span>${timer--}</span>`
     console.log(timer)
 }
