920cec2b94aa184aa231ad70710b7bd631f2f0a2
LAK <email@lak.com>
2022-02-16 19:53:37 -0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const interval = document.querySelector('.interval')
 const startBtn = document.querySelector('.start-btn')
+const stopBtn = document.querySelector('.stop-btn')
+const modal = document.querySelector('.modal')
+const time = document.querySelector('.time')
+
+startBtn.addEventListener('click', () => {
+    // time.innerHTML = interval.value
+    
+    modal.classList.toggle('change')
+})
+
+stopBtn.addEventListener('click', () => {
+    modal.classList.toggle('change')
+})
+
