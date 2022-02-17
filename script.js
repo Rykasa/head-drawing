ba1cd1ce33d7c83b8fed9252a81d86b50d26973d
LAK <email@lak.com>
2022-02-17 16:07:40 -0300

    update

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
+const references = [
+    {
+        id: 0,
+        src: "mikasa.jpg",
+        view: "3/4"
+    },
+    {
+        id: 1,
+        src: "ley-bowen-.jpg",
+        view: "3/4"
+    }
+]
+
 const interval = document.querySelector('.interval')
 const startBtn = document.querySelector('.start-btn')
 const stopBtn = document.querySelector('.stop-btn')
 const modal = document.querySelector('.modal')
 const time = document.querySelector('.time')
 const img = document.querySelector('.reference-img')
+const pauseBtn = document.querySelector('.pause-btn')
+const skipBtn = document.querySelector('.skip-btn')
 
 startBtn.addEventListener('click', () => {
-    // time.innerHTML = interval.value
     modal.classList.toggle('change')
+    changeReference()
 })
 
 stopBtn.addEventListener('click', () => {
     modal.classList.toggle('change')
+    duration = timer
 })
 
-const timer = parseInt(interval.value / 100) * 60 + interval.value % 100
+skipBtn.addEventListener('click', () =>{
+    changeReference()
+    duration = timer
+    startTimer()
+})
 
 //Conversão para segundos
+let timer = parseInt(interval.value / 100) * 60 + interval.value % 100
 let duration = timer
-
 let minutes
 let seconds
 
@@ -34,6 +54,7 @@ function startTimer(){
     }
 
     if(seconds < 0){
+        changeReference()
         duration = timer
         setTimer()
         time.textContent = minutes + ":" + seconds
@@ -51,7 +72,9 @@ function setTimer(){
 }
 
 function changeReference(){
-    
+    img.src = references[Math.floor(Math.random() * references.length)].src
 }
 
 setInterval(startTimer, 1000)
+
+console.log(references)
